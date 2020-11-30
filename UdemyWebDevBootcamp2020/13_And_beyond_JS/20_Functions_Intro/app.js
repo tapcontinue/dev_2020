2e89e84ab2cc8b3683102650a6bf6bd18a7012d8
Noel Tanner <noeltanner@gmail.com>
2020-11-30 09:36:33 -0600

    Updates up to section 208 quiz 5.

--- UdemyWebDevBootcamp2020/13_And_beyond_JS/20_Functions_Intro/app.js
+++ UdemyWebDevBootcamp2020/13_And_beyond_JS/20_Functions_Intro/app.js
@@ -1,3 +1,5 @@
+//* 206. Arguments Intro
+/*
 function singSong() {
     console.log("DO");
     console.log("RE");
@@ -8,6 +10,10 @@ function greet(firstName, lastName) {
     console.log(`Hey there, ${firstName} ${lastName[0]}.`)
 }
 
+*/
+
+//* 207. Functions With Multiple Arguments
+/*
 function repeat(str, numTimes) {
     let result = '';
     for (let i = 0; i < numTimes; i++) {
@@ -22,20 +28,88 @@ function add(x, y) {
     }
     return x + y;
 }
+*/
 
+//* 208. The Return Keyword
 
+/*
+// * Ex 1
+function add(x, y) {
+    return x + y;
+}
 
+const sum = add(10, 16)
+const answer = add(100, 200)
 
+console.log(`The sum is ${sum} and the answer is ${answer}`)
+*/
 
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
+// *Ex 2 - Bypass returns with conditional block
+//* Return is how we get values out of a function. Save & capture.
 
+// function add(x, y) {
+//     if (typeof x !== 'number' || typeof  y !== 'number'){
+//         return false;
+//     }
+//     return (x + y)
+// }
+//
+// add(8, 11)
+// add(8, 11)
+
+// !QUIZ
+
+// * Quiz 1
+// too easy to include
+
+// * Quiz 2
+// function isShortsWeather(temperature){
+//     if (typeof temperature !== 'number'){
+//         console.log('Try again, temp must be number')
+//     } else if(temperature >= 75){
+//         return true;
+//     } else{
+//         return false;
+//     }
+// }
+//
+// isShortsWeather(74);
+
+//* quiz 3
+// function lastElement(lastItem) {
+//     if (lastItem ==! true){
+//         return null;
+//     }
+//     return lastItem.slice(-1)[0];
+// }
+//
+// console.log(lastElement([3, 5, 7]))
+// console.log(lastElement([1]))
+// console.log(lastElement([]))
+
+// * Quiz 4
+
+// function capitalize(word) {
+//     return word[0].toUpperCase() + word.slice(1);
+// }
+//
+// console.log(capitalize('eggplant'))
+
+// * Quiz 5
+
+function sumArray(amount) {
+    let total = 0;
+    for (let i = 0; i < amount.length; ++i) {
+        total += amount[i];
+    }
+    return total;
+}
+
+console.log(sumArray([1, 2, 3]))
+console.log(sumArray([2, 2, 2, 2]))
+console.log(sumArray([50, 50, 1]))
+
+// * Quiz 6
+// function return day() {
+//
+// }
